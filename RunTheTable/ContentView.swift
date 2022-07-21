734d31739321e0a46ba6bfd4be840a91c42deac6
Ian Jackson <iansjackson8@gmail.com>
2022-07-21 12:36:46 -0400

    Initial Commit

---
+++ RunTheTable/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  RunTheTable
+//
+//  Created by Ian Jackson on 7/21/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RunTheTable/RunTheTableApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunTheTableApp.swift
+//  RunTheTable
+//
+//  Created by Ian Jackson on 7/21/22.
+//
+
+import SwiftUI
+
+@main
+struct RunTheTableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
